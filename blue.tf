aa2e7fd59b5a6e1f2f774fc4de47fa9785ec73aa
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-01-11 07:47:08 -0800

    update blue instances to display version

--- blue.tf
+++ blue.tf
@@ -1,16 +1,16 @@
 resource "aws_instance" "blue" {
-  count = var.enable_green_env ? var.blue_instance_count : 0
+  count = var.enable_blue_env ? var.blue_instance_count : 0
 
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
   subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
   vpc_security_group_ids = [module.app_security_group.this_security_group_id]
   user_data = templatefile("${path.module}/init-script.sh", {
-    file_content = "blue #${count.index}"
+    file_content = "version 1.0 #${count.index}"
   })
 
   tags = {
-    Name = "blue-${count.index}"
+    Name = "version-1.0-${count.index}"
   }
 }
 
