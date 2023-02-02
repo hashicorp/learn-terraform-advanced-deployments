02a41fc2a4752c726b546dc677b3907e1ef98bb8
Alan Szlosek <alan.szlosek@hashicorp.com>
2023-02-02 16:45:44 -0500

    m1 support; exclude opt-in AZs

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {
   state = "available"
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
 module "vpc" {
